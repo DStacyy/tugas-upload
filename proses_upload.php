2ecc4215ee51ef185572b0a7d272d5b1bd73b30e
DStacyy <gabutajajirlah@gmail.com>
2026-03-09 10:55:20 +0700

    if else

--- proses_upload.php
+++ proses_upload.php
@@ -11,6 +11,20 @@ $ekstensiValid = ['jpg', 'png', 'jpeg'];
 $ekstensi = strtolower(pathinfo($mamaFile, PATHINFO_EXTENSION));
 if(!in_array($ekstensi, $ekstensiValid)){
     echo "<div class='alert alert-danger'>Ekstensi tidak valid</div>";
-    exit();
+    exit;
+}
+if($ukuran > 1000000) {
+    echo "<div class='alert alert-danger'>Ukuran file terlalu besar</div>";
+    exit;
+}
+
+//rename fiilel agar namanya jadi unik
+$namaBaru = uniqid() . "." . $ekstensi;
+
+if(move_uploaded_file($tmp, $folder . $namaBaru)){
+    echo "<div class='alert alert-sucsess'>File berhasil diupload</div>";
+    echo "<img src='$folder$namaBaru' width='300' class='img-thumbnail'>"
+}else{
+    echo "<div class='alert alert-danger'>Gagal upload filer</div>";
 }
 ?>
