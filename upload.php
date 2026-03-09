c914733bde73f0932fdfd2a2e037adfcbecde0b5
DStacyy <gabutajajirlah@gmail.com>
2026-03-09 10:13:06 +0700

    initial

---
+++ upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Gambar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+<body class="container">
+    <div class="container">
+        <h2>Upload Gambar</h2>
+
+        <form action="proses_upload.php" method="post" enctype="multipart/form-data"></form>
+
+
+    </div>
+</body>
+</html>
