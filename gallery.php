65fa4e00552ab12cc03d39fa1b6e55742feb8691
DStacyy <gabutajajirlah@gmail.com>
2026-03-10 07:31:31 +0700

    work on gallery.php

---
+++ gallery.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery Gambar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body class="p-4">
+    <div class="container">
+        <h2>Gallery Gambar</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-3 mb-3">
+            <img src="uploads/69af5c446edd8.jpg" alt="Gambar">
+        </div>
+    </div>
+</body>
+</html>
